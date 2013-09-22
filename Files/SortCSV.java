340203f8ff6cf101ef1d0c744f22e9f12fc32539
RomanGotsiy <gotsijroman@gmail.com>
2013-09-22 12:49:52 +0000

    Added Sort CSV programm

---
+++ Files/SortCSV.java
@@ -0,0 +1,148 @@
+/**Sort Excel/CSV File Utility - Reads a file of records, 
+ * sorts them, and then writes them back to the file. Allow 
+ * the user to choose various sort style and sorting based 
+ * on a particular field.
+ * 
+ * @author Roman Gotsiy (gotsijroman@gmail.com)
+ * */
+
+import java.io.*;
+import java.util.*;
+
+class CSVFile {
+	
+	private List<String[]> records;
+	
+	//constants for sortDirection
+	final public int SortASC = 1;
+	final public int SortDESC = -1;
+	private int sortDirection = SortASC; //1 for ASC, -1 for DESC	
+	String fileName;
+	
+	private int colsCount=0;
+	
+	public CSVFile(String file) throws IOException{
+		records = new ArrayList<String[]>();
+		fileName = file;
+		BufferedReader in = new BufferedReader(new FileReader(file));
+		try {
+			String ln;
+			while( (ln = in.readLine()) !=null) {
+				colsCount = ln.split("\t").length; //FIXME
+				records.add(ln.split("\t"));
+			}
+		}
+		finally {
+			in.close();
+		}
+	}
+	
+	public void print(){
+		for(String[] arr : records){
+			for (String s:arr) {
+				System.out.print(s+"\t");
+			}
+			System.out.println();
+		}
+	}
+	
+	public void save() throws IOException{
+		BufferedWriter out = new BufferedWriter(new FileWriter(fileName));
+		
+		try {
+			for(String[] arr : records){
+				for (String s:arr) {
+					out.write(s+"\t");
+				}
+				out.write("\n");
+			}
+		}
+		finally {
+			out.close();
+		}
+	}
+	
+	public void setSortDirection(int direction){
+		sortDirection = direction;
+	}
+	
+	public void sortByCol(final int i){
+		
+		//comparator by specific col
+		Comparator<String[]> comp = new Comparator<String[]>(){
+			public int compare(String[] a, String[] b){
+				//reverse result if DESC (sortDirection = -1)
+				return sortDirection * a[i].compareTo(b[i]);
+			}
+		};
+		
+		Collections.sort(records, comp);
+	}
+	
+	public int getColsCount(){
+		return colsCount;
+	}
+}
+
+
+
+class SortCSV {
+	private static BufferedReader in;
+	
+	private static String fileName;
+	
+	public static void main(String args[]) throws IOException{
+		
+		in = new BufferedReader(new InputStreamReader(System.in));
+		if (args.length<1){
+			//promt user for input file
+			System.out.println("Enter path to .CSV file: ");
+			fileName = in.readLine();
+		} else
+		{
+			fileName = args[0];
+		}	
+		
+		try {
+			CSVFile csv = new CSVFile(fileName);
+		
+		
+			csv.print();
+			
+			int maxcol = csv.getColsCount();
+			System.out.println(
+					String.format("Select sorting column (1-%d): [1] ",maxcol));
+			
+			int sortCol = 1;
+			String res =in.readLine();
+			
+			//if non-default
+			if (res.trim().length() != 0){
+				sortCol = Integer.parseInt(res);
+				if ((sortCol<1) || (sortCol >maxcol)){
+					System.out.println("Incorect column number");
+					System.exit(0);
+				}
+			}
+			
+			String sortDirection = "1";
+			System.out.println("Select sort direction");
+			System.out.println("1. ASC");
+			System.out.println("2. DESC");
+			System.out.print("[1]:");
+			sortDirection = in.readLine();
+			if (sortDirection == "2")
+				csv.setSortDirection(csv.SortDESC);
+			
+			csv.sortByCol(sortCol-1); //-1 map from 1:n to 0:n-1
+			csv.save();
+			System.out.println("Sorted and saved to file");
+			System.out.println("Would you like to see result before exit? (yes/no)[no]");
+			if (in.readLine().trim().equals("yes"))
+				csv.print();
+		} catch(IOException e) {
+			System.out.println("File doesn't exist");
+			System.exit(0);
+		}
+	}
+}
